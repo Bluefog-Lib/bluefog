a95e3a15c9b85d78fcfef87f3bfe6208f6fb5f69
ybc <bichengying@gmail.com>
2019-12-31 20:54:09 -0800

    Update profile script

--- scripts/run_profile.sh
+++ scripts/run_profile.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 now=$(date +"%T")
+mnist=./examples/pytorch_mnist.py
+prof_file=./examples/.profile/mnist.prof
+mkdir ./examples/.profile
 
-mpirun -np 4 python -m cProfile -o ./.profile/bf_program.prof as_gan_mpi.py --skip_tensorboard --epoch=2
+mpirun -np 4 python -m cProfile -o  $prof_file $mnist --epoch=3
 
 # Run it on local machine
-# snakeviz ./.profile/program.prof
+snakeviz $prof_file
