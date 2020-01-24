ab664fab747b1bd96f7592f0f3c59b28434c638c
ybc <bichengying@gmail.com>
2020-01-23 23:39:02 -0800

    Add more functionality into bfrun

--- bluefog/run/run.py
+++ bluefog/run/run.py
@@ -1,5 +1,30 @@
 import argparse
+import os
+import shlex
+import subprocess
+import sys
+import traceback
+
+
 import bluefog
+
+
+def _is_open_mpi_installed():
+    command = 'mpirun --version'
+    try:
+        output_msg = str(subprocess.check_output(
+            shlex.split(command), universal_newlines=True))
+    except Exception:
+        print("Was not able to run %s:\n%s" % (command, output_msg),
+              file=sys.stderr)
+        print(traceback.format_exc(), file=sys.stderr)
+        return False
+
+    if 'Open MPI' not in output_msg:
+        print('Open MPI not found in output of mpirun --version.',
+              file=sys.stderr)
+        return False
+    return True
 
 
 def parse_args():
@@ -34,12 +59,7 @@ def parse_args():
 
     parsed_args = parser.parse_args()
 
-    if parsed_args.verbose is None:
-        # This happens if the user does "bfrun --verbose" without
-        # specifying any value to verbose. For the sake of consistency, we set
-        # the verbosity here to the default value of 1.
-        parsed_args.verbose = 1
-
+    parsed_args.verbose = 2 if parsed_args.verbose else 1
     if not parsed_args.version and not parsed_args.np:
         parser.error('argument -np/--num-proc is required')
 
@@ -51,6 +71,49 @@ def main():
     if args.version:
         print(bluefog.__version__)
         exit(0)
+
+    if args.host:
+        hosts_arg = args.host
+    else:
+        hosts_arg = ""
+        
+    if args.ssh_port:
+        ssh_port_arg = "-mca plm_rsh_args \"-p {ssh_port}\"".format(
+            ssh_port=args.ssh_port)
+    else:
+        ssh_port_arg = ""
+
+    if not _is_open_mpi_installed():
+        raise Exception(
+            'bfrun convenience script currently only supports Open MPI.\n\n'
+            'Choose one of:\n'
+            '1. Install Open MPI 4.0.0+ and re-install Horovod '
+            '(use --no-cache-dir pip option).\n'
+            '2. Run distributed '
+            'training script using the standard way provided by your'
+            ' MPI distribution (usually mpirun, srun, or jsrun).')
+
+    # Pass all the env variables to the mpirun command.
+    env = os.environ.copy()
+
+    mpirun_command = (
+        'mpirun --allow-run-as-root --tag-output '
+        '-np {num_proc} {hosts_arg} '
+        '-bind-to none -map-by slot '
+        '-mca pml ob1 -mca btl ^openib '
+        '{ssh_port_arg} '
+        '{env} {command}'  # expect a lot of environment variables
+        .format(num_proc=args.np,
+                hosts_arg=hosts_arg,
+                ssh_port_arg=ssh_port_arg,
+                env='',
+                command=' '.join(shlex.quote(par) for par in args.command))
+    )
+
+    if args.verbose >= 2:
+        print(mpirun_command)
+    # Execute the mpirun command.
+    os.execve('/bin/sh', ['/bin/sh', '-c', mpirun_command], env)
 
 
 if __name__ == "__main__":
