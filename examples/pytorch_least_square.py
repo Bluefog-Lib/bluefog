cedfad508b4e53c494d5aecc46d5d719afd93ef6
ybc <bichengying@gmail.com>
2020-04-11 11:51:28 -0700

    Update pytorch_least_square.py

--- examples/pytorch_least_square.py
+++ examples/pytorch_least_square.py
@@ -14,20 +14,19 @@
 # ==============================================================================
 
 import torch
-import numpy as np
 import bluefog.torch as bf
 from bluefog.common import topology_util
-import copy
 
 bf.init()
 
-# The least squares problem is min_x \sum_i^n \|A_i x - b_i\|^2 
+# The least squares problem is min_x \sum_i^n \|A_i x - b_i\|^2
 # where each rank i holds A_i and b_i
 # we expect each rank will converge to the global solution after the algorithm
 
 # Generate data
 # y = A@x + ns where ns is Gaussion noise
-m, n = 5, 3
+torch.random.manual_seed(123417 * bf.rank())
+m, n = 10, 3
 A = torch.randn(m, n)
 x = torch.randn(n, 1)
 ns = 0.1*torch.randn(m, 1)
@@ -36,8 +35,8 @@ b = A.mm(x) + ns
 # Calculate the true solution with distributed gradient descent
 # x^{k+1} = x^k - alpha * \sum_i A_i.T(A_i x - b_i)
 x = torch.zeros(n, 1)
-maxite = 1000
-alpha = 1e-1
+maxite = 2000
+alpha = 5e-2
 for i in range(maxite):
     grad = A.T.mm(A.mm(x) - b)                  # local gradient
     grad = bf.allreduce(grad, name='gradient')  # global gradient
@@ -45,13 +44,13 @@ for i in range(maxite):
 
 # evaluate the convergence of distributed least-squares
 # the norm of global gradient is expected to 0 (optimality condition)
-print("Rank {}: global gradient norm: {}".format(bf.rank(), \
-        torch.norm(bf.allreduce(A.T.mm(A.mm(x) - b)))))
+global_grad_norm = torch.norm(bf.allreduce(A.T.mm(A.mm(x) - b)), p=2)
+print("Rank {}: global gradient norm: {}".format(bf.rank(), global_grad_norm))
 
 # the norm of local gradient is expected not be be close to 0
 # this is because each rank converges to global solution, not local solution
-print("Rank {}: local gradient norm: {}".format(bf.rank(), \
-        torch.norm(A.T.mm(A.mm(x) - b))))
+local_grad_norm = torch.norm(A.T.mm(A.mm(x) - b), p=2)
+print("Rank {}: local gradient norm: {}".format(bf.rank(), local_grad_norm))
 
 # Calculate the true solution with exact diffusion:
 # Reference: https://arxiv.org/abs/1702.05122
@@ -60,7 +59,7 @@ print("Rank {}: local gradient norm: {}".format(bf.rank(), \
 # x^{k+1} = neighbor_allreduce(phi^{k+1})
 x, phi = torch.zeros(n, 1), torch.zeros(n, 1)
 psi, psi_prev = torch.zeros(n, 1), torch.zeros(n, 1)
-alpha_ed = 1e-1 # step-size for exact diffusion
+alpha_ed = 5e-2  # step-size for exact diffusion
 for i in range(maxite):
     grad = A.T.mm(A.mm(x)-b)    # local gradient
     psi = x - alpha * grad
@@ -70,10 +69,10 @@ for i in range(maxite):
 
 # evaluate the convergence of exact diffuion least-squares
 # the norm of global gradient is expected to 0 (optimality condition)
-print("Rank {}: global gradient norm: {}".format(bf.rank(), \
-        torch.norm(bf.allreduce(A.T.mm(A.mm(x) - b)))))
+global_grad_norm = torch.norm(bf.allreduce(A.T.mm(A.mm(x) - b)), p=2)
+print("Rank {}: global gradient norm: {}".format(bf.rank(), global_grad_norm))
 
 # the norm of local gradient is expected not be be close to 0
 # this is because each rank converges to global solution, not local solution
-print("Rank {}: local gradient norm: {}".format(bf.rank(), \
-        torch.norm(A.T.mm(A.mm(x) - b))))
+local_grad_norm = torch.norm(A.T.mm(A.mm(x) - b), p=2)
+print("Rank {}: local gradient norm: {}".format(bf.rank(), local_grad_norm))
