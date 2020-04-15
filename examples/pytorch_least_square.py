d4d7b84fc6ff2a1dc79d8880c4b6415f4dc5e7d1
Kun Yuan <kunyuan827@gmail.com>
2020-04-16 00:46:36 +0800

    add push-DIGing

--- examples/pytorch_least_square.py
+++ examples/pytorch_least_square.py
@@ -47,12 +47,12 @@ for i in range(maxite):
 # evaluate the convergence of distributed least-squares
 # the norm of global gradient is expected to 0 (optimality condition)
 global_grad_norm = torch.norm(bf.allreduce(A.T.mm(A.mm(x_opt) - b)), p=2)
-print("Rank {}: global gradient norm: {}".format(bf.rank(), global_grad_norm))
+print("[DG] Rank {}: global gradient norm: {}".format(bf.rank(), global_grad_norm))
 
 # the norm of local gradient is expected not be be close to 0
 # this is because each rank converges to global solution, not local solution
 local_grad_norm = torch.norm(A.T.mm(A.mm(x_opt) - b), p=2)
-print("Rank {}: local gradient norm: {}".format(bf.rank(), local_grad_norm))
+print("[DG] Rank {}: local gradient norm: {}".format(bf.rank(), local_grad_norm))
 
 # Calculate the true solution with exact diffusion:
 # Reference: https://arxiv.org/abs/1702.05122
@@ -75,12 +75,12 @@ for i in range(maxite):
 # evaluate the convergence of exact diffuion least-squares
 # the norm of global gradient is expected to be 0 (optimality condition)
 global_grad_norm = torch.norm(bf.allreduce(A.T.mm(A.mm(x) - b)), p=2)
-print("Rank {}: global gradient norm: {}".format(bf.rank(), global_grad_norm))
+print("[ED] Rank {}: global gradient norm: {}".format(bf.rank(), global_grad_norm))
 
 # the norm of local gradient is expected not be be close to 0
 # this is because each rank converges to global solution, not local solution
 local_grad_norm = torch.norm(A.T.mm(A.mm(x) - b), p=2)
-print("Rank {}: local gradient norm: {}".format(bf.rank(), local_grad_norm))
+print("[ED] Rank {}: local gradient norm: {}".format(bf.rank(), local_grad_norm))
 
 
 if bf.rank() == 0:
@@ -108,13 +108,69 @@ for i in range(maxite):
 # evaluate the convergence of gradient tracking for least-squares
 # the norm of global gradient is expected to be 0 (optimality condition)
 global_grad_norm = torch.norm(bf.allreduce(A.T.mm(A.mm(x) - b)), p=2)
-print("Rank {}: global gradient norm: {}".format(bf.rank(), global_grad_norm))
+print("[GT] Rank {}: global gradient norm: {}".format(bf.rank(), global_grad_norm))
 
 # the norm of local gradient is expected not be be close to 0
 # this is because each rank converges to global solution, not local solution
 local_grad_norm = torch.norm(A.T.mm(A.mm(x) - b), p=2)
-print("Rank {}: local gradient norm: {}".format(bf.rank(), local_grad_norm))
+print("[GT] Rank {}: local gradient norm: {}".format(bf.rank(), local_grad_norm))
 
 if bf.rank() == 0:
     plt.semilogy(mse_gt)
+    plt.show()
+
+# Calculate the true solution with Push-DIGing:
+# Reference: https://arxiv.org/abs/1607.03218
+bf.set_topology(topology_util.PowerTwoRingGraph(bf.size()))
+outdegree = len(bf.out_neighbor_ranks())
+indegree = len(bf.in_neighbor_ranks())
+
+# u, y, v = w[:n], w[n:2*n], w[2n]
+w = torch.zeros(2*n+1, 1).to(torch.double)
+x = torch.zeros(n, 1).to(torch.double)
+w[n:2*n] = A.T.mm(A.mm(x)-b)
+w[-1] = 1.0
+grad_prev = w[n:2*n].clone()
+
+bf.win_create(w, name="w_buff", zero_init=True)
+
+alpha_pd = 1e-2  # step-size for GT (should be smaller than exact diffusion)
+mse_pd = []
+maxite = 1000
+for i in range(maxite):
+    if i%10 == 0:
+        bf.barrier()
+
+    w[:n] = w[:n] - alpha_pd*w[n:2*n]
+    bf.win_accumulate(
+        w, name="w_buff",
+        dst_weights={rank: 1.0 / (outdegree + 1)
+                     for rank in bf.out_neighbor_ranks()},
+        require_mutex=True)
+    w.div_(1+outdegree)
+    w = bf.win_sync_then_collect(name="w_buff")
+
+    x = w[:n]/w[-1]
+    grad = A.T.mm(A.mm(x)-b)
+    w[n:2*n] += grad - grad_prev
+    grad_prev = grad
+    if bf.rank() == 0:
+        mse_pd.append(torch.norm(x - x_opt, p=2))
+
+bf.barrier()
+w = bf.win_sync_then_collect(name="w_buff")
+x = w[:n]/w[-1]
+
+# evaluate the convergence of gradient tracking for least-squares
+# the norm of global gradient is expected to be 0 (optimality condition)
+global_grad_norm = torch.norm(bf.allreduce(A.T.mm(A.mm(x) - b)), p=2)
+print("[PD] Rank {}: global gradient norm: {}".format(bf.rank(), global_grad_norm))
+
+# the norm of local gradient is expected not be be close to 0
+# this is because each rank converges to global solution, not local solution
+local_grad_norm = torch.norm(A.T.mm(A.mm(x) - b), p=2)
+print("[PD] Rank {}: local gradient norm: {}".format(bf.rank(), local_grad_norm))
+
+if bf.rank() == 0:
+    plt.semilogy(mse_pd)
     plt.show()
