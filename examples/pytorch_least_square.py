0721ba492334d4179e5b25df6b00a9365f469d87
kunyuan <kunyuan@g.ucla.edu>
2020-05-09 00:11:33 +0800

    Update pytorch_least_square.py
    add argparse

--- examples/pytorch_least_square.py
+++ examples/pytorch_least_square.py
@@ -17,6 +17,17 @@ import torch
 import bluefog.torch as bf
 from bluefog.common import topology_util
 import matplotlib.pyplot as plt
+import argparse
+
+# Parser
+parser = argparse.ArgumentParser(
+    description="PyTorch ImageNet Example",
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+)
+parser.add_argument(
+    "--method", type=int, default=0, help="0:exact diffusion. 1:gradient tracking. 2:push-DIGing"
+)
+args = parser.parse_args()
 
 bf.init()
 
@@ -71,34 +82,34 @@ print("[DG] Rank {}: local gradient norm: {}".format(bf.rank(), local_grad_norm)
 # [R2] Z. Li, W. Shi and M. Yan, ``A Decentralized Proximal-gradient Method with
 #  Network Independent Step-sizes and Separated Convergence Rates'', 2019
 # ================================================================================
-x = torch.zeros(n, 1).to(torch.double)
-phi, psi, psi_prev = x.clone(), x.clone(), x.clone()
-alpha_ed = 1e-2  # step-size for exact diffusion
-mse = []
-for i in range(maxite):
-    grad = A.T.mm(A.mm(x)-b)    # local gradient
-    psi = x - alpha * grad
-    phi = psi + x - psi_prev
-    x = bf.neighbor_allreduce(phi, name='local variable')
-    psi_prev = psi
+if args.method == 0:
+    x = torch.zeros(n, 1).to(torch.double)
+    phi, psi, psi_prev = x.clone(), x.clone(), x.clone()
+    alpha_ed = 1e-2  # step-size for exact diffusion
+    mse = []
+    for i in range(maxite):
+        grad = A.T.mm(A.mm(x)-b)    # local gradient
+        psi = x - alpha * grad
+        phi = psi + x - psi_prev
+        x = bf.neighbor_allreduce(phi, name='local variable')
+        psi_prev = psi
+        if bf.rank() == 0:
+            mse.append(torch.norm(x - x_opt, p=2))
+
+    # evaluate the convergence of exact diffuion least-squares
+    # the norm of global gradient is expected to be 0 (optimality condition)
+    global_grad_norm = torch.norm(bf.allreduce(A.T.mm(A.mm(x) - b)), p=2)
+    print("[ED] Rank {}: global gradient norm: {}".format(
+        bf.rank(), global_grad_norm))
+
+    # the norm of local gradient is expected not be be close to 0
+    # this is because each rank converges to global solution, not local solution
+    local_grad_norm = torch.norm(A.T.mm(A.mm(x) - b), p=2)
+    print("[ED] Rank {}: local gradient norm: {}".format(bf.rank(), local_grad_norm))
+
     if bf.rank() == 0:
-        mse.append(torch.norm(x - x_opt, p=2))
-
-# evaluate the convergence of exact diffuion least-squares
-# the norm of global gradient is expected to be 0 (optimality condition)
-global_grad_norm = torch.norm(bf.allreduce(A.T.mm(A.mm(x) - b)), p=2)
-print("[ED] Rank {}: global gradient norm: {}".format(
-    bf.rank(), global_grad_norm))
-
-# the norm of local gradient is expected not be be close to 0
-# this is because each rank converges to global solution, not local solution
-local_grad_norm = torch.norm(A.T.mm(A.mm(x) - b), p=2)
-print("[ED] Rank {}: local gradient norm: {}".format(bf.rank(), local_grad_norm))
-
-
-if bf.rank() == 0:
-    plt.semilogy(mse)
-    plt.show()
+        plt.semilogy(mse)
+        plt.show()
 
 # ======================= gradient tracking =====================================
 # Calculate the true solution with gradient tracking (GT for short):
@@ -120,33 +131,34 @@ if bf.rank() == 0:
 # [R4] P. Di Lorenzo and G. Scutari, ``Next: In-network nonconvex optimization'',
 # 2016
 # ================================================================================
-x = torch.zeros(n, 1).to(torch.double)
-y = A.T.mm(A.mm(x)-b)
-grad_prev = y.clone()
-alpha_gt = 5e-3  # step-size for GT (should be smaller than exact diffusion)
-mse_gt = []
-for i in range(maxite):
-    x = bf.neighbor_allreduce(x, name='local variable x') - alpha_gt * y
-    grad = A.T.mm(A.mm(x)-b)    # local gradient at x^{k+1}
-    y = bf.neighbor_allreduce(y, name='local variable y') + grad - grad_prev
-    grad_prev = grad
+if args.method == 1:
+    x = torch.zeros(n, 1).to(torch.double)
+    y = A.T.mm(A.mm(x)-b)
+    grad_prev = y.clone()
+    alpha_gt = 5e-3  # step-size for GT (should be smaller than exact diffusion)
+    mse_gt = []
+    for i in range(maxite):
+        x = bf.neighbor_allreduce(x, name='local variable x') - alpha_gt * y
+        grad = A.T.mm(A.mm(x)-b)    # local gradient at x^{k+1}
+        y = bf.neighbor_allreduce(y, name='local variable y') + grad - grad_prev
+        grad_prev = grad
+        if bf.rank() == 0:
+            mse_gt.append(torch.norm(x - x_opt, p=2))
+
+    # evaluate the convergence of gradient tracking for least-squares
+    # the norm of global gradient is expected to be 0 (optimality condition)
+    global_grad_norm = torch.norm(bf.allreduce(A.T.mm(A.mm(x) - b)), p=2)
+    print("[GT] Rank {}: global gradient norm: {}".format(
+        bf.rank(), global_grad_norm))
+
+    # the norm of local gradient is expected not be be close to 0
+    # this is because each rank converges to global solution, not local solution
+    local_grad_norm = torch.norm(A.T.mm(A.mm(x) - b), p=2)
+    print("[GT] Rank {}: local gradient norm: {}".format(bf.rank(), local_grad_norm))
+
     if bf.rank() == 0:
-        mse_gt.append(torch.norm(x - x_opt, p=2))
-
-# evaluate the convergence of gradient tracking for least-squares
-# the norm of global gradient is expected to be 0 (optimality condition)
-global_grad_norm = torch.norm(bf.allreduce(A.T.mm(A.mm(x) - b)), p=2)
-print("[GT] Rank {}: global gradient norm: {}".format(
-    bf.rank(), global_grad_norm))
-
-# the norm of local gradient is expected not be be close to 0
-# this is because each rank converges to global solution, not local solution
-local_grad_norm = torch.norm(A.T.mm(A.mm(x) - b), p=2)
-print("[GT] Rank {}: local gradient norm: {}".format(bf.rank(), local_grad_norm))
-
-if bf.rank() == 0:
-    plt.semilogy(mse_gt)
-    plt.show()
+        plt.semilogy(mse_gt)
+        plt.show()
 
 # ======================= Push-DIGing for directed graph =======================
 # Calculate the true solution with Push-DIGing:
@@ -156,58 +168,59 @@ if bf.rank() == 0:
 # [R1] A. Nedic, A. Olshevsky, and W. Shi, ``Achieving geometric convergence
 # for distributed optimization over time-varying graphs'', 2017. (Alg. 2)
 # ============================================================================
-bf.set_topology(topology_util.PowerTwoRingGraph(bf.size()))
-outdegree = len(bf.out_neighbor_ranks())
-indegree = len(bf.in_neighbor_ranks())
+if args.method == 2:
+    bf.set_topology(topology_util.PowerTwoRingGraph(bf.size()))
+    outdegree = len(bf.out_neighbor_ranks())
+    indegree = len(bf.in_neighbor_ranks())
 
-# u, y, v = w[:n], w[n:2*n], w[2n]
-w = torch.zeros(2*n+1, 1).to(torch.double)
-x = torch.zeros(n, 1).to(torch.double)
-w[n:2*n] = A.T.mm(A.mm(x)-b)
-w[-1] = 1.0
-grad_prev = w[n:2*n].clone()
+    # u, y, v = w[:n], w[n:2*n], w[2n]
+    w = torch.zeros(2*n+1, 1).to(torch.double)
+    x = torch.zeros(n, 1).to(torch.double)
+    w[n:2*n] = A.T.mm(A.mm(x)-b)
+    w[-1] = 1.0
+    grad_prev = w[n:2*n].clone()
 
-bf.win_create(w, name="w_buff", zero_init=True)
+    bf.win_create(w, name="w_buff", zero_init=True)
 
-# step-size for Push-DIGing (should be smaller than exact diffusion)
-alpha_pd = 1e-2
-mse_pd = []
-maxite = 1000
-for i in range(maxite):
-    if i % 10 == 0:
-        bf.barrier()
+    # step-size for Push-DIGing (should be smaller than exact diffusion)
+    alpha_pd = 1e-2
+    mse_pd = []
+    maxite = 1000
+    for i in range(maxite):
+        if i % 10 == 0:
+            bf.barrier()
 
-    w[:n] = w[:n] - alpha_pd*w[n:2*n]
-    bf.win_accumulate(
-        w, name="w_buff",
-        dst_weights={rank: 1.0 / (outdegree + 1)
-                     for rank in bf.out_neighbor_ranks()},
-        require_mutex=True)
-    w.div_(1+outdegree)
+        w[:n] = w[:n] - alpha_pd*w[n:2*n]
+        bf.win_accumulate(
+            w, name="w_buff",
+            dst_weights={rank: 1.0 / (outdegree + 1)
+                        for rank in bf.out_neighbor_ranks()},
+            require_mutex=True)
+        w.div_(1+outdegree)
+        w = bf.win_sync_then_collect(name="w_buff")
+
+        x = w[:n]/w[-1]
+        grad = A.T.mm(A.mm(x)-b)
+        w[n:2*n] += grad - grad_prev
+        grad_prev = grad
+        if bf.rank() == 0:
+            mse_pd.append(torch.norm(x - x_opt, p=2))
+
+    bf.barrier()
     w = bf.win_sync_then_collect(name="w_buff")
-
     x = w[:n]/w[-1]
-    grad = A.T.mm(A.mm(x)-b)
-    w[n:2*n] += grad - grad_prev
-    grad_prev = grad
+
+    # evaluate the convergence of gradient tracking for least-squares
+    # the norm of global gradient is expected to be 0 (optimality condition)
+    global_grad_norm = torch.norm(bf.allreduce(A.T.mm(A.mm(x) - b)), p=2)
+    print("[PD] Rank {}: global gradient norm: {}".format(
+        bf.rank(), global_grad_norm))
+
+    # the norm of local gradient is expected not be be close to 0
+    # this is because each rank converges to global solution, not local solution
+    local_grad_norm = torch.norm(A.T.mm(A.mm(x) - b), p=2)
+    print("[PD] Rank {}: local gradient norm: {}".format(bf.rank(), local_grad_norm))
+
     if bf.rank() == 0:
-        mse_pd.append(torch.norm(x - x_opt, p=2))
-
-bf.barrier()
-w = bf.win_sync_then_collect(name="w_buff")
-x = w[:n]/w[-1]
-
-# evaluate the convergence of gradient tracking for least-squares
-# the norm of global gradient is expected to be 0 (optimality condition)
-global_grad_norm = torch.norm(bf.allreduce(A.T.mm(A.mm(x) - b)), p=2)
-print("[PD] Rank {}: global gradient norm: {}".format(
-    bf.rank(), global_grad_norm))
-
-# the norm of local gradient is expected not be be close to 0
-# this is because each rank converges to global solution, not local solution
-local_grad_norm = torch.norm(A.T.mm(A.mm(x) - b), p=2)
-print("[PD] Rank {}: local gradient norm: {}".format(bf.rank(), local_grad_norm))
-
-if bf.rank() == 0:
-    plt.semilogy(mse_pd)
-    plt.show()
+        plt.semilogy(mse_pd)
+        plt.show()
