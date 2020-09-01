3abd2a07a316db0e7e5c510e52612decede59113
ybc <bichengying@gmail.com>
2020-08-31 20:58:46 -0700

    minor update for NCCLContext without const

--- bluefog/common/nccl_controller.h
+++ bluefog/common/nccl_controller.h
@@ -147,7 +147,7 @@ class NCCLContext {
 };
 
 // Function to implement Window 
-void WinPassiveRecvRequest(int self_rank, const NCCLContext& nccl_ctx);
+void WinPassiveRecvRequest(int self_rank, NCCLContext& nccl_ctx);
 
 class NCCLController {
  public:
