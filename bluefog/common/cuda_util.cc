7a714337c6c231beba889cf855fb3713c4f44743
Bluefog-Lib <65107588+Bluefog-Lib@users.noreply.github.com>
2021-12-12 21:19:15 -0800

    Fix the cuda stream creation in MPI (#101)
    * Fix the cuda stream creation no matter the MPI using with CPU tensor
    only or not.
    
    * Remove the skip of test_neighbor_allreduce_dst_weight_fusion
    
    Co-authored-by: ybc <bichengying@gmail.com>

--- bluefog/common/cuda_util.cc
+++ bluefog/common/cuda_util.cc
@@ -18,26 +18,14 @@
 #include "cuda_runtime.h"
 #endif
 
-#include <stdio.h>
 #include <assert.h>
+#include <stdio.h>
 
-#include "logging.h"
 #include "cuda_util.h"
+#include "logging.h"
 
 namespace bluefog {
 namespace common {
-
-#if HAVE_CUDA
-#define CUDACHECK(ans) { CudaAssert((ans), __FILE__, __LINE__); }
-inline void CudaAssert(cudaError_t code, const char *file, int line, bool abort=true)
-{
-   if (code != cudaSuccess) 
-   {
-      fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
-      if (abort) exit(code);
-   }
-}
-#endif
 
 with_device::with_device(int device) {
   if (device == CPU_DEVICE_ID) {
@@ -47,8 +35,9 @@ with_device::with_device(int device) {
     CUDACHECK(cudaGetDevice(&restore_device_));
     CUDACHECK(cudaSetDevice(device));
 #else
-    throw std::logic_error("Internal error. Requested device context manager "
-                           "with GPU device but not compiled with CUDA.");
+    throw std::logic_error(
+        "Internal error. Requested device context manager "
+        "with GPU device but not compiled with CUDA.");
 #endif
   }
 }
@@ -61,5 +50,5 @@ with_device::~with_device() {
 #endif
 }
 
-} // namespace common
-} // namespace bluefog
+}  // namespace common
+}  // namespace bluefog
